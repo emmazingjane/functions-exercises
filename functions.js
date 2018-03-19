e97356da9544003da4c704777136a5ec3f144034
emmabennett <emma.j.bennett15@gmail.com>
2018-03-19 14:16:58 -0700

    first commit of JS functions 2

--- functions.js
+++ functions.js
@@ -1,1 +1,20 @@
 // TODO: add your solutions here!
+
+
+
+function toNthPower(number, power) {
+	var totalCount = 1;
+	for (var i = 0; i < number; i++) {
+		number
+	}
+}
+
+function printTriangle(length) {
+	var star = '*';
+	for (var i = 0; i < length; i++) {
+		star += '*';
+		console.log(star);
+
+		
+	}
+}
