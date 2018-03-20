67452210b4b5e5d5cd5639788bdf2fea87e87b75
emmabennett <emma.j.bennett15@gmail.com>
2018-03-19 19:53:41 -0700

    finished the function problems

--- functions.js
+++ functions.js
@@ -1,6 +1,19 @@
 // TODO: add your solutions here!
 
+function combineWords(word1, word2){
+	var newString = word1 + word2
+}
 
+var newString = combineWords('dog', 'house');
+console.log(newString);
+
+
+function mutiplePhrases(phrase, howManyTimes){
+	for (var i=0; i < howManyTimes; i++) {
+		var count = phrase
+		console.log(count);
+	}
+}
 
 function toNthPower(number, power) {
 	var totalCount = 1;
@@ -9,12 +22,58 @@ function toNthPower(number, power) {
 	}
 }
 
+function findArea(radius) {
+	let diameter = 2 * radius;
+	let findArea = 3.14 * diameter;
+	console.log(findArea);
+
+}
+
+c = 3.14 * d
+diameter = 2 * r
+
+
+function findC(a, b) {
+	let aS = Math.pow(a, 2);
+	let bS = Math.pow(b, 2);
+	let cS = aS + bS;
+	console.log(cS);
+}
+findC(3,6);
+
+
+function isXEvenlyDivisibleByY(x, y) {
+	var result = x % y === 0;
+	return result;
+}
+
+
+function howManyVowels(word){
+	let vowelsInWord = 0;
+	for (var i = 0; i = word.length; i++) {
+		if (word[i] === 'a' || word[i] === 'e' || word[i] === 'i' || word[i] === 'o' || word[i] === 'u') {
+		vowelsInWord++
+	}
+		
+	}
+}
+var result = howManyVowels('emmazing');
+console.log(result);
+
+function findWdi(array){
+	for (var i = 0; i = array.length; i++) {
+		if (i === 'wdi') {
+			return true;
+		}
+	}
+}
+
+
 function printTriangle(length) {
 	var star = '*';
 	for (var i = 0; i < length; i++) {
 		star += '*';
 		console.log(star);
-
 		
 	}
 }
